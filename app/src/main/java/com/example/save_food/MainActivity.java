3f011b539b0a1b9011bb839647aa120933d9272f
NDuyPhuc <nguyenduyphuc0119@gmail.com>
2023-07-22 16:16:48 +0700

    commit

--- app/src/main/java/com/example/save_food/MainActivity.java
+++ app/src/main/java/com/example/save_food/MainActivity.java
@@ -1,14 +1,37 @@
 package com.example.save_food;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+    Button upload;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AnhXa();
+        btn();
+
+    }
+    //khi người dùng ấn vào dấu "+"
+    public void btn(){
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, UploadActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    //ánh xạ
+    public void AnhXa(){
+        upload = findViewById(R.id.upload);
     }
 }
