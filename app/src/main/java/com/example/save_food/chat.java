6dd13ca8facba6f251c664a6a87d11b573928460
QuyenWibu <91710203+QuyenWibu@users.noreply.github.com>
2023-08-02 12:13:58 +0700

    fix

--- app/src/main/java/com/example/save_food/chat.java
+++ app/src/main/java/com/example/save_food/chat.java
@@ -111,7 +111,7 @@ public class chat extends AppCompatActivity {
         recyclerView = findViewById(R.id.chatrecycle);
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(linearLayoutManager);
-        apiService = Client.getRetrofit("https://fcm.googleapis.com/fcm/send").create(APIService.class);
+        apiService = Client.getRetrofit("https://fcm.googleapis.com/").create(APIService.class);
         Intent intent = getIntent();
         hisUid = getIntent().getStringExtra("hisUid");
 
